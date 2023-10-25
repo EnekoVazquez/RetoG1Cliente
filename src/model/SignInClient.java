f091ffca507247bcbc3dabb0a77d32c303fb0a10
josu <2dam@HZ375750.TartangaLH.eus>
2023-10-25 12:04:56 +0200

    sign in version 1.3(final?)

--- src/model/SignInClient.java
+++ src/model/SignInClient.java
@@ -5,10 +5,25 @@
  */
 package model;
 
+import exception.CredentialErrorException;
+import exception.ServerErrorException;
+import exception.UserAlreadyExistsException;
+import exception.UserNotFoundException;
+
 /**
  *
- * @author Eneko.
+ * @author .
  */
-public class SignInClient {
+public class SignInClient implements Sign{
+
+    @Override
+    public User getExecuteSignUp(User user) throws ServerErrorException, UserAlreadyExistsException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User getExecuteSignIn(User user) throws ServerErrorException, CredentialErrorException, UserNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
